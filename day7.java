1195b088b9c065572e768795d66cb2bf5c0314e8
Reignnnn <dagumreigngaebriell@gmail.com>
2025-09-21 20:48:52 +0800

    Initial Java practice files

---
+++ day7.java
@@ -0,0 +1,45 @@
+
+//insertion sort
+
+import java.util.Scanner;
+
+public class day7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("How many numbers do you want to enter?: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        // input numbers
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter number " + (i + 1) + ": ");
+            arr[i] = sc.nextInt();
+        }
+
+        // call insertion sort
+        insertionSort(arr);
+
+        // output sorted array
+        System.out.println("\nSorted numbers:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+
+    // insertion sort method
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+}
