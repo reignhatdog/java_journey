b36f3a4c979a77c06268aa2982b00ca8e827b159
Reignnnn <dagumreigngaebriell@gmail.com>
2025-10-04 19:45:46 +0800

    day09.java

--- day09.java
+++ day09.java
@@ -2,7 +2,6 @@
 
 
 import java.util.Scanner;
-
 public class day9 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -43,4 +42,5 @@ public class day9 {
         }
     }
 }
+
 
