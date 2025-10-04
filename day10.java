06300db7b268c3e4cddeaccb35e577186bac5bcf
Reignnnn <dagumreigngaebriell@gmail.com>
2025-10-04 19:46:07 +0800

    day10.java

--- day10.java
+++ day10.java
@@ -2,7 +2,6 @@
 
 public class day10 {
     public static void main(String[] args) {
-
         int n = 5;
     for (int i = 1; i <= n; i++) {
     for (int j = 1; j <= n - i; j++) System.out.print(" ");
@@ -15,3 +14,4 @@ public class day10 {
     
     }}
   
+
